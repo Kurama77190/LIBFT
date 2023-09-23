4e9e17e7a4b77789c9504b18c6e3da85ef8d54b9
Samy PC PORTABLE <samy@DESKTOP-0QSI7UJ.localdomain>
2023-09-23 03:02:39 +0200

    update

--- libft_projects/libft/ft_substr.c
+++ libft_projects/libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 18:37:36 by sben-tay          #+#    #+#             */
-/*   Updated: 2023/09/16 18:37:50 by sben-tay         ###   ########.fr       */
+/*   Updated: 2023/09/23 03:01:44 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-		
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (ft_strlen(s) < start)
+		len = 0;
+	if (ft_strlen(s + start) < len)
+		len = ft_strlen(s + start);
+	str = malloc(sizeof(*s) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
 }
