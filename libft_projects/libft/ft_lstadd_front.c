75f547f0b032841170052dd9bdbfc465f4dd00b2
Samy Ben Tayeb <sben-tay@made-f0Cr11s5.clusters.42paris.fr>
2023-11-10 16:37:25 +0100

    LSTADDFRONT OK

---
+++ libft_projects/libft/ft_lstadd_front.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 15:06:54 by sben-tay          #+#    #+#             */
+/*   Updated: 2023/11/10 16:15:51 by sben-tay         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
+
+
+// int main ()
+// {
+// 	char *c = "nn";
+// 	char *d = "ouiii";
+// 	char *e = "NTM";
+// 	char *f = "GG";
+	
+// 	t_list *lst;
+// 	lst = malloc(sizeof(t_list));
+// 	t_list *new;
+// 	new = malloc(sizeof(t_list));
+// 	new->content = f;
+
+// 	lst->content = c;
+// 	lst->next = ft_lstnew(d);
+// 	lst->next->next = ft_lstnew(e);
+// 	printf("%s\n",(char *)lst->content);
+// 	printf("%s\n",(char *)lst->next->content);
+// 	printf("%s\n",(char *)lst->next->next->content);
+// 	printf("\n");
+// 	printf("\n");
+// 	ft_lstadd_front(&lst, new);
+// 	printf("%s\n",(char *)lst->content);
+// 	printf("%s\n",(char *)lst->next->content);
+// 	printf("%s\n",(char *)lst->next->next->content);
+// 	printf("%s\n",(char *)lst->next->next->next->content);
+// }
