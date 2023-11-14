4fa0a4d12485336ccfe4550ac9b431c480c1b4f4
Kurama77190 <samybentayeb@outlook.com>
2023-11-14 04:17:10 +0100

    :heavy_check_mark:

--- libft_projects/libft/ft_memcmp.c
+++ libft_projects/libft/ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sben-tay <sben-tay@student.42.paris.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/12 17:02:47 by sben-tay          #+#    #+#             */
-/*   Updated: 2023/09/12 17:44:39 by sben-tay         ###   ########.fr       */
+/*   Created: 2023/11/14 04:06:31 by sben-tay          #+#    #+#             */
+/*   Updated: 2023/11/14 04:06:32 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t				i;
 
 	i = 0;
-	str = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
+	str = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	if (!s1 || !s2)
+		return (0);
 	if (n == 0)
 		return (0);
-	while (str[i] == str2[i] && i < n - 1)
+	while (i < n - 1 && str[i] == str2[i])
 	{
 		i++;
 	}
 	if (str[i] == str2[i])
 		return (0);
 	else
-		return ((unsigned char *)str + i - (unsigned char *)str2 + i);
+		return (str[i] - str2[i]);
 }
--- libft_projects/libft/ft_substr.c
+++ libft_projects/libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sben-tay <sben-tay@student.42.paris.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/16 18:37:36 by sben-tay          #+#    #+#             */
-/*   Updated: 2023/09/23 03:01:44 by sben-tay         ###   ########.fr       */
+/*   Created: 2023/11/14 04:08:41 by sben-tay          #+#    #+#             */
+/*   Updated: 2023/11/14 04:08:42 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,50 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	char	*str;
+	char	*dest;
 
-	if (ft_strlen(s) < start)
-		len = 0;
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
-	str = malloc(sizeof(*s) * (len + 1));
-	if (!str)
+	if (!s || start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	dest = malloc(sizeof(char) * (len + 1));
+	if (!dest)
 		return (NULL);
-	i = 0;
+	i = start ;
 	j = 0;
-	while (s[i])
+	while (j < len && s[i])
 	{
-		if (i >= start && j < len)
-		{
-			str[j] = s[i];
-			j++;
-		}
+		dest[j] = s[i];
+		j++;
 		i++;
 	}
-	str[j] = 0;
-	return (str);
+	dest[j] = '\0';
+	return (dest);
 }
+
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	char	*str;
+
+// 	if (ft_strlen(s) < start)
+// 		len = 0;
+// 	if (ft_strlen(s + start) < len)
+// 		len = ft_strlen(s + start);
+// 	str = malloc(sizeof(*s) * (len + 1));
+// 	if (!str)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (s[i])
+// 	{
+// 		if (i >= start && j < len)
+// 		{
+// 			str[j] = s[i];
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// 	str[j] = 0;
+// 	return (str);
+// }
