31c8108f7d12e7c1c534b338cc93359f066cb719
Kurama77190 <samybentayeb@outlook.com>
2023-11-18 09:37:31 +0100

    :chart:

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sben-tay <sben-tay@student.42.paris.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 04:06:31 by sben-tay          #+#    #+#             */
-/*   Updated: 2023/11/17 14:12:53 by sben-tay         ###   ########.fr       */
+/*   Updated: 2023/11/18 00:30:50 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	str = (const unsigned char *)s1;
 	str2 = (const unsigned char *)s2;
-	if (!s1 && !s2)
+	if ((!s1 && !s2) || n == 0)
 		return (0);
 	while (i < n - 1 && str[i] == str2[i])
 	{
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,30 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sben-tay <sben-tay@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sben-tay <sben-tay@student.42.paris.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 04:07:06 by sben-tay          #+#    #+#             */
-/*   Updated: 2023/11/17 13:32:07 by sben-tay         ###   ########.fr       */
+/*   Updated: 2023/11/18 00:51:59 by sben-tay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_putnbr(int n, int fd)
+{
+	long int	nb;
+
+	nb = (long int)n;
+	if (nb == -2147483648)
+	{
+		ft_putchar_fd ('-', fd);
+		ft_putchar_fd ('2', fd);
+		nb = 147483648;
+	}
+	if (nb >= 0 && nb <= 9)
+	{
+		ft_putchar_fd (nb + 48, fd);
+	}
+	if (nb < 0)
+	{
+		write(fd, "-", 1);
+		ft_putnbr (nb * (-1), fd);
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr (nb / 10, fd);
+		ft_putnbr (nb % 10, fd);
+	}
+	return (0);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_putnbr_fd(n / 10, fd);
-	}
-	ft_putchar_fd((n % 10) + '0', fd);
+	ft_putnbr (n, fd);
 }
+
+// int main ()
+// {
+// 	ft_putnbr_fd(-2147483648, 2);
+// }	
